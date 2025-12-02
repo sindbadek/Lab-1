5e67a96e7468b61cae5e0d45bf025608cdcfe0ac
sindbadek <142395074+sindbadek@users.noreply.github.com>
2025-12-02 14:47:44 +0000

    More complication validation

--- main.tf
+++ main.tf
@@ -3,7 +3,23 @@ type = string
 description = "Name of server to proviusion"
 }
 
+locals {
+  minNumberServer =1
+  maxNumberServer =10
+}
+
 
 output "out" {
   value = var.server-name
 }
+
+variable "number-of-servers" {
+    type = number
+    description = "requier number of servers"
+  validation {
+    condition = var.number-of-servers>=local.minNumberServer && var.number-of-servers<local.maxNumberServer
+        error_message = "Not supported, it should be from the range [${local.minNumberServer},${local.maxNumberServer}]"
+  }
+}
+
+
