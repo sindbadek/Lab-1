a588343b725c8e7aff519d3ac0f261ddaf89e926
sindbadek <142395074+sindbadek@users.noreply.github.com>
2025-12-02 14:58:41 +0000

    Kolejne zmiany

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ type = string
 description = "Name of server to proviusion"
 }
 
+variable "list-of-names" {
+  type = list (string)
+}
+
 locals {
   minNumberServer =1
   maxNumberServer =10
@@ -10,12 +14,13 @@ locals {
 
 
 output "out" {
-  value = var.server-name
+  value = "${var.server-name} x ${var.number-of-servers}"
 }
 
 variable "number-of-servers" {
     type = number
     description = "requier number of servers"
+    default = 2
   validation {
     condition = var.number-of-servers>=local.minNumberServer && var.number-of-servers<local.maxNumberServer
         error_message = "Not supported, it should be from the range [${local.minNumberServer},${local.maxNumberServer}]"
