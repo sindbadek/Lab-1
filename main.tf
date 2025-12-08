7229f42ea8040851df02fe2f577d36d59889c976
sindbadek <142395074+sindbadek@users.noreply.github.com>
2025-12-08 20:10:52 +0000

    Pliki

--- main.tf
+++ main.tf
@@ -1,50 +1,37 @@
-# main.tf - kalkulator Terraform z obsługą dzielenia przez 0
-
-variable "liczba1" {
-  description = "Pierwsza liczba"
-  type        = number
-#  default     = 10
-}
-
-variable "liczba2" {
-  description = "Druga liczba"
-  type        = number
-#  default     = 2
-}
-
-variable "operacja" {
-  description = "Operacja: '+', '-', '*', '/'"
-  type        = string
- # default     = "*"
-
-  validation {
-    condition     = contains(["+", "-", "*", "/"], var.operacja)
-    error_message = "Operacja musi być jedną z: '+', '-', '*', '/'."
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.25.0"
+    }
   }
 }
-
-locals {
-  # Wszystkie operacje w oddzielnych locals (bezpieczne typy number)
-  dodawanie         = var.liczba1 + var.liczba2
-  odejmowanie       = var.liczba1 - var.liczba2
-  mnozenie          = var.liczba1 * var.liczba2
-  dzielenie_bezpieczne = var.liczba2 != 0 ? var.liczba1 / var.liczba2 : 999999
-  
-  # JEDNA LINIA - główny wynik
-  wynik = var.operacja == "+" ? local.dodawanie : var.operacja == "-" ? local.odejmowanie : var.operacja == "*" ? local.mnozenie : local.dzielenie_bezpieczne
-  
-  # Status jako string (typ string)
-  status = var.operacja == "/" && var.liczba2 == 0 ? "DZIELENIE PRZEZ 0!" : "OK"
+ 
+provider "aws" {
+  # Configuration options
 }
 
-output "wynik" {
-  value = local.wynik
+variable "bucket_prefix" {
+  description = "Prefiks dla nazwy bucketu"
+  type        = string
+  default     = "awsninja7-"
 }
+ 
+ resource "aws_s3_bucket" "my_bucket" {
+    bucket_prefix = var.bucket_prefix
+ }
 
-output "status_operacji" {
-  value = local.status
-}
+ output "bucket-name" {
+   value = aws_s3_bucket.my_bucket.bucket
+ }
 
-output "szczegoly" {
-  value = "L1=${var.liczba1} L2=${var.liczba2} OP=${var.operacja} W=${local.wynik}"
+
+resource "aws_s3_object" "object" {
+for_each = toset(["plik","plik2"])
+
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "${each.key}/"
+source = null
+content = ""
+
 }
