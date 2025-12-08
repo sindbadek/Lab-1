c054e532200f20c83c9e27a33b93849b3e3af974
sindbadek <142395074+sindbadek@users.noreply.github.com>
2025-12-08 20:48:53 +0000

    Tagi

--- main.tf
+++ main.tf
@@ -11,14 +11,26 @@ provider "aws" {
   # Configuration options
 }
 
+# Inupt - Bucket prefix jako zmienna + jako domyślny jest ustawiony Twój
 variable "bucket_prefix" {
   description = "Prefiks dla nazwy bucketu"
   type        = string
   default     = "awsninja7-"
 }
- 
+
+# Inupt - Tagi, gdzie domyślne jest w variable "puropse": "learning" (moze być nadpisany)
+variable "purpose" {
+  description = "Cel użycia"
+  type        = string
+  default     = "learning"  # Domyślne
+}
+
  resource "aws_s3_bucket" "my_bucket" {
     bucket_prefix = var.bucket_prefix
+      tags = {
+    owner   = "so"
+    purpose = var.purpose  
+  }
  }
 
  output "bucket-name" {
@@ -30,8 +42,8 @@ resource "aws_s3_object" "object" {
 for_each = toset(["plik","plik2"])
 
   bucket = aws_s3_bucket.my_bucket.bucket
-  key    = "${each.key}/"
-source = null
-content = ""
+  key    = "${each.key}"
+  source = ".terraform/test/${each.key}" 
+#content = ""
 
 }
