baad6a4bc917b8a7fb9cfa503088a3298a11e2ac
sindbadek <142395074+sindbadek@users.noreply.github.com>
2025-12-08 21:23:09 +0000

    wszystkie zadania

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ terraform {
 provider "aws" {
   # Configuration options
 }
+data "aws_region" "current" {}
 
 # Inupt - Bucket prefix jako zmienna + jako domyślny jest ustawiony Twój
 variable "bucket_prefix" {
@@ -19,24 +20,52 @@ variable "bucket_prefix" {
 }
 
 # Inupt - Tagi, gdzie domyślne jest w variable "puropse": "learning" (moze być nadpisany)
+# Zawsze dodany tag do bucketu (lub wszędzie) "owner": "jw"
 variable "purpose" {
   description = "Cel użycia"
   type        = string
   default     = "learning"  # Domyślne
 }
 
+locals {
+  # Tag owner zawsze obecny - nie do nadpisania
+  mandatory_tags = {
+    owner = "so"
+  }
+  
+  # Tagi nadpisywalne (purpose itp.)
+  optional_tags =  {
+    purpose = var.purpose
+  }
+}
+
+
+
+
  resource "aws_s3_bucket" "my_bucket" {
     bucket_prefix = var.bucket_prefix
-      tags = {
-    owner   = "so"
-    purpose = var.purpose  
-  }
+#      tags = {
+#    owner   = "so"
+#    purpose = var.purpose  
+#  }
+tags = merge(local.mandatory_tags, local.optional_tags)
  }
 
  output "bucket-name" {
    value = aws_s3_bucket.my_bucket.bucket
  }
 
+# Outputs: bucket ARN oraz pełny URL z HTTP
+output "bucket_arn" {
+  description = "ARN utworzonego bucketu"
+  value       = aws_s3_bucket.my_bucket.arn
+}
+
+output "bucket_http_url" {
+  description = "Pełny URL HTTP do bucketu"
+  value       = "http://${aws_s3_bucket.my_bucket.bucket}.s3.${data.aws_region.current.name}.amazonaws.com"
+  #http://awsninja7-20251208201944409100000001.s3-website-us-east-1.amazonaws.com
+}
 
 resource "aws_s3_object" "object" {
 for_each = toset(["plik","plik2"])
